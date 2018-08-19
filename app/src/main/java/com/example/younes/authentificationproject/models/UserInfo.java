2f9c4284764f61b5d19822e608d8bde1d923c337
younesthestar <younes.najjar.96@gmail.com>
2018-08-19 05:38:15 +0100

    Added Retrofit First Connection

---
+++ app/src/main/java/com/example/younes/authentificationproject/models/UserInfo.java
@@ -0,0 +1,8 @@
+package com.example.younes.authentificationproject.models;
+
+/**
+ * Created by younes on 8/19/2018.
+ */
+
+public class UserInfo {
+}
---
+++ app/src/main/java/com/example/younes/authentificationproject/utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.younes.authentificationproject.utils;
+
+/**
+ * Created by younes on 8/19/2018.
+ */
+
+class AppConstants {
+}
